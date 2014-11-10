4f55a0547c973ae9e9e16f48088ed238330c9a0a
Maya Brandi <maya.brandi@tools.scilifelab.se>
2014-11-10 11:00:40 +0100

    changing comment

--- scripts/load_dup_remQC.py
+++ scripts/load_dup_remQC.py
@@ -4,23 +4,26 @@ import json
 from argparse import ArgumentParser
 
 def main(stat_file, single, min, proj):
+    min_reads_aft_dup_rem = (float(min)/2.0)
     qc_dict = {}
     dict = eval(open('stat.json').read())
     for key, val in dict.items():
         if single:
-            M_reads_aft_dup_rem = round(float(val['aft_dup_rem']['mapq >= mapq_cut (unique)'])/1000000.0,2)
+            M_reads_aft_dup_rem = round(float(
+                   val['aft_dup_rem']['mapq >= mapq_cut (unique)'])/1000000.0,2)
         else:
             R1 = val['aft_dup_rem']['Read-1']
             R2 = val['aft_dup_rem']['Read-2']
             M_reads_aft_dup_rem = round((float(R2)+float(R1))/2000000,2)
-        if M_reads_aft_dup_rem > (float(min)/2.0):
+        if M_reads_aft_dup_rem > min_reads_aft_dup_rem:
             qc_passed = True
         else:
             qc_passed = False
-        print "{0}  : {1} : {2} : {3}".format(key, qc_passed ,str(M_reads_aft_dup_rem), min)
+        print "{0}  : {1} : {2} : {3}".format(key, qc_passed ,
+                                             str(M_reads_aft_dup_rem), str(min))
         qc_dict[key] = {"automated_qc": {
                                 "qc_passed": qc_passed,
-                                "qc_reason": "M reads after duplicates removed: {0}. Minimal amount: {1}".format(str(M_reads_aft_dup_rem), min)}}
+                                "qc_reason": "M reads after duplicates removed: {0}. Minimal amount after duplicates removed: {1}".format(str(M_reads_aft_dup_rem), min_reads_aft_dup_rem)}}
     f_path = '/proj/a2012043/private/nobackup/app_QC/'
     j = json.dumps(qc_dict)
     f = open(f_path + proj + '.json', 'w')
@@ -34,7 +37,7 @@ if __name__ == "__main__":
                         help='stat.json file - output from rna-seq pipe')
     parser.add_argument('-s', default = False, action="store_true", dest = 'single',
                         help=('Single end reads.'))
-    parser.add_argument('-m', default = False, dest = 'min',
+    parser.add_argument('-m', default = False, dest = 'min', type = float,
                             help=('Min reads (M)'))
     parser.add_argument('-p', default = False, dest = 'proj',
                                  help=('project name'))
