22d8f57120703d0ca99a499d29a09adc05258089
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2015-09-11 15:05:43 +0200

    CPU hours and Storage usage script update
    	done: 	added --config argument (path to db config file)
    		fixed a few small errors
    		output logging info

--- scripts/quota_log.py
+++ scripts/quota_log.py
@@ -6,12 +6,25 @@ import datetime
 import subprocess
 import couchdb
 import yaml
+import argparse
+import os
+import logging
+import sys
+
+
+DEFAULT_CONFIG = os.path.expanduser("~/.ngi_config/statusdb.yaml")
 
 def disk_quota():
     current_time = datetime.datetime.now()
-    uq = subprocess.Popen(["/bubo/sw/uppmax/bin/uquota", "-q"], stdout=subprocess.PIPE)
+    try:
+        uq = subprocess.Popen(["/sw/uppmax/bin/uquota", "-q"], stdout=subprocess.PIPE)
+    except Exception, e:
+        logging.error(e.message)
+        raise
 
     output = uq.communicate()[0]
+    logging.info("Disk Usage:")
+    logging.info(output)
 
     projects = output.split("\n/proj/")[1:]
 
@@ -35,12 +48,19 @@ def disk_quota():
 
 def cpu_hours():
     current_time = datetime.datetime.now()
-
-    # script that runs on uppmax
-    uq = subprocess.Popen(["/sw/uppmax/bin/projinfo", '-q'], stdout=subprocess.PIPE)
+    try:
+        # script that runs on uppmax
+        uq = subprocess.Popen(["/sw/uppmax/bin/projinfo", '-q'], stdout=subprocess.PIPE)
+    except Exception, e:
+        logging.error(e.message)
+        raise
 
     # output is lines with the format: project_id  cpu_usage  cpu_limit
     output = uq.communicate()[0]
+
+    logging.info("CPU Hours Usage:")
+    logging.info(output)
+
     result = {}
     # parsing output
     for proj in output.strip().split('\n'):
@@ -71,25 +91,54 @@ def save_results(disk_quota_data, cpu_hours_data, db_config):
             merged_results[project] = cpu_hours_data[project]
 
 
+    logging.info("Connecting to the Database: {url}".format(url=db_config['url']))
     # create db instance
     server = "http://{username}:{password}@{url}:{port}".format(
         url=db_config['url'],
         username=db_config['username'],
         password=db_config['password'],
         port=db_config['port'])
+    try:
+        couch = couchdb.Server(server)
+    except Exception, e:
+        logging.error(e.message)
+        raise
 
-    couch = couchdb.Server(server)
     db = couch['uppmax']
+    logging.info('Connection established')
 
     # save results in the database
     for project in merged_results.values():
-        db.save(project)
+        try:
+            project_id, project_rev = db.save(project)
+        except Exception, e:
+            logging.error(e.message)
+            raise
+        else:
+            logging.info('Project: {id} {name} has been updated'.format(id=project_id, name=project['project']))
 
 
 
 if __name__ == "__main__":
-    with open('/home/funk_001/.ngi_config/statusdb.yaml', 'r') as config_file:
-        config = yaml.load(config_file.readall())
+    # parse arguments from command line
+    parser = argparse.ArgumentParser(description="Formats uquota \
+        information as a dict, and sends it to a given CouchDB.")
+
+    parser.add_argument("--config", dest="config", default=DEFAULT_CONFIG, \
+        help="Path to the DB config file.")
+
+    parser.add_argument("--log-level", dest="log_level", default="error", help="Log level: debug, info, error, critical")
+    parser.add_argument("--log-file", dest="log_file", default=sys.stdout, help="Path to log file")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(format="", levelname=args.log_level, filename=args.log_file)
+    try:
+        with open(args.config, 'r') as config_file:
+            config = yaml.load(config_file.read())
+    except IOError, e:
+        logging.error(e.message)
+        raise
 
     disk_quota_dict = disk_quota()
     cpu_hours_dict = cpu_hours()
